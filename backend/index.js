20ef9b0d52a0fa61338020854fe7b43cba16540a
anmol <anmolkrgupta1885@gmail.com>
2026-04-10 13:10:54 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
+import connectionRouter from "./routes/connection.routes.js";
+import http from "http";
+import { Server } from "socket.io";
+import notificationRouter from "./routes/notification.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/connection", connectionRouter);
+app.use("/api/notification", notificationRouter);
+
+export const userSocketMap = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("register", (userId) => {
+    userSocketMap.set(userId, socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    for (let [key, value] of userSocketMap.entries()) {
+      if (value === socket.id) {
+        userSocketMap.delete(key);
+      }
+    }
+  });
+});
+
+/* ---------- STATIC FRONTEND SERVING ---------- */
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+/* --------------------------------------------- */
+
+server.listen(port, () => {
+    console.log(process.env.PORT)
+  connectDb();
+  console.log("server started");
+});
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react'
+export const authDataContext=createContext()
+function AuthContext({children}) {
+const serverUrl = import.meta.env.VITE_SERVER_URL
+    let value={
+        serverUrl
+    }
+  return (
+    <div>
+     <authDataContext.Provider value={value}> 
+     {children}
+     </authDataContext.Provider> 
+    </div>
+  )
+}
+
+export default AuthContext
